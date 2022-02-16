13713cda5f7c655300c865c7738cf9220973a490
Abeartale <marcosndietz@gmail.com>
2022-02-16 18:17:22 -0300

    Spring boot project creation

---
+++ to-do-list-rest-api/src/main/java/com/ensolvers/todolistrestapi/ToDoListRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.ensolvers.todolistrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListRestApiApplication.class, args);
+	}
+
+}
---
+++ to-do-list-rest-api/src/test/java/com/ensolvers/todolistrestapi/ToDoListRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensolvers.todolistrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
